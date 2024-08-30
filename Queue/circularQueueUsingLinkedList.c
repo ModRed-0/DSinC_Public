8d75041c372a3776fa5668b82f53654a60d172b5
ModRed <127257957+ModRed-0@users.noreply.github.com>
2024-08-30 21:19:15 +0530

    Update circularQueueUsingLinkedList.c

--- Queue/circularQueueUsingLinkedList.c
+++ Queue/circularQueueUsingLinkedList.c
@@ -12,18 +12,51 @@
 
     starting mei both front and end point to NULL since queue is empty
 
+    {NOTE: not the literal function arguement here, just for simpler illustration}
+    enqueue(5) will result in;
+
+    (f)5(e)
+    ^    |
+    |____|
+
+    front and end both are at 5 only.
+
+    wow, that was hard to make. yeah, im !not making more diagrams.
+
+    enqueue(10); front is 5 and end is 10
+    (f)5 --> (e)10
+    ^            |
+    |____________|
+
+    enqueue(15);
+    (f)5 --> 10 --> 15(e)
+    ^                |
+    |________________|
+
+    dequeue();
+    (f)10 --> 15(e)
+    ^            |
+    |____________|
+
+    dequeue();
+    (f)15(e)
+    ^     |
+    |_____|
+
+    and so on.......
     
-    peek() and traverse() mere hisab se is very intuitive and need not be explained.
+
+    peek() and traversal() mere hisab se is very intuitive and need not be explained.
     __________________________________________________________________________________
 
     Problems I faced implementing this? 
-    None tbh. Quite easy to implement if you are comfy with Single Linked List.
+    since i directly jumped to circular LL without studying the theory first
+    took me some time to figure things out by trial and error, but overall no issues tbh.
 
     Improvements can be done by listing elements that have been dequeued or enqueued, 
     also better documentations maybe? lol.
 
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -33,10 +66,10 @@ typedef struct node
     struct node* next;
 }node;
 
-void enqueue(node** front, node **end);
-void dequeue(node** front, node **end);
-void traversal(node* front, node *end);
-void peek(node* front);
+void enqueue(node** front, node **end); //inserts element from end
+void dequeue(node** front, node **end); //deletes element from start
+void traversal(node* front, node *end); //traverses the queue
+void peek(node* front); //front element that will be dequeued if needed.
 
 int main()
 {
@@ -89,15 +122,20 @@ void enqueue(node** front, node **end)
     {   
         *end = (node*)malloc(sizeof(node));
         (*end)->data = data;
+        //if queue is empty then end node will point to itself
         (*end)->next = *end;
+        //front will point to end since 1 node he hai queue mei
         *front = *end;
         return;
     } 
+    //making a new node here below
     node* newNode = (node*)malloc(sizeof(node));
     newNode->data = data;
+    //yaha pe removing the circular property of end-->front and putting end --> newNode connection below
     (*end)->next = newNode;
+    //yaha pe reintroducing the circular property below newNode will now point to front
     newNode->next = *front;
-    *end = newNode; //error was here, rectified now.
+    *end = newNode; //error was here, rectified now. update end to newNode
 }
 void dequeue(node** front, node **end)
 {   
@@ -108,16 +146,20 @@ void dequeue(node** front, node **end)
     }
 
     if(*front == *end)
-    {
+    {   
+        //dequeueing when only 1 element there.
         free(*front);
         *front = NULL;
         *end = NULL;
         return;
     }
-
+    //temp to free the node and delete it
     node* temp = (*front);
+    //updating front to the next node
     *front = (*front)->next;
+    //end->next should point to the new front since the circular LL was updated (1st node deleted)
     (*end)->next = *front;
+    //temp delete kar diya and dangling pointer handle kar diya
     free(temp);
     temp = NULL;
 }
@@ -129,6 +171,8 @@ void traversal(node* front, node *end)
         return;
     }
     node* temp = front;
+    //took some time to decide on do..while instead of just while, did weird shit with while() loop
+    //but nhi hua while se bas. i think the flag method would work, but kaun karega ab? try kar lo khud se
     do
     {   
         printf("%d",temp->data);
